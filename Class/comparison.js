75ad74c042a8d25c338d3053667c2fd2939c4d6f
Aman <amanmulla4020@gmail.com>
2026-01-18 22:55:49 +0530

    EXperiment no 1 and some class work

---
+++ Class/comparison.js
@@ -0,0 +1,28 @@
+console.log(2>=1);
+console.log(2>1);
+
+console.log(2<1);
+console.log(2<=1);
+console.log(2==1);
+console.log(2!=1);
+console.log("------------");
+
+console.log("2">1);
+console.log("02">1);//this type comparison is not standard and less chances to get correct output
+
+console.log(null > 0);
+console.log(null== 0);
+console.log(null >= 0);
+console.log( undefined== 0);
+console.log(undefined>=0);
+
+console.log("2"===2);//check condition and datatype
+
+
+
+
+
+
+
+
+
